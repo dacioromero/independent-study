40245755a6e0316856fcca1239b8122df58939ff
FireClawGames <Dacio_Romero@outlook.com>
2016-04-18 13:36:00 -0400

    Still working

---
+++ Test/src/org/hackerrank/java/introduction/EndOffFile.java
@@ -0,0 +1,18 @@
+package org.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class EndOffFile
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        for(int i = 1; scanner.hasNextLine(); i++)
+        {
+        	System.out.printf("%d %s%n", i, scanner.nextLine());
+        }
+        
+        scanner.close();
+    }
+}
