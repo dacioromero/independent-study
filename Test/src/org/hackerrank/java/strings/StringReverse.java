0cdd8fd03798be1ace3742dee452308b3f69d502
Dacio Romero <Dacio_Romero@outlook.com>
2016-04-25 07:33:07 -0400

    Working on string reversal.

--- Test/src/org/hackerrank/java/strings/StringReverse.java
+++ Test/src/org/hackerrank/java/strings/StringReverse.java
@@ -1,6 +1,15 @@
 package org.hackerrank.java.strings;
 
+import java.util.Scanner;
+
 public class StringReverse
 {
-
+	public static void main(String[] args)
+	{
+        Scanner scanner = new Scanner(System.in);
+        
+        StringBuilder string = new StringBuilder(scanner.next());
+        
+        scanner.close();
+	}
 }
