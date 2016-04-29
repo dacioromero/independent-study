07d52bc809e1005b708a2e16e4f5e1eca680d4c1
FireClawGames <Dacio_Romero@outlook.com>
2016-04-29 13:34:31 -0400

    Still working on Tag Content Extractor

--- Test/src/org/hackerrank/java/strings/TagContentExtractor.java
+++ Test/src/org/hackerrank/java/strings/TagContentExtractor.java
@@ -1,6 +1,7 @@
 package org.hackerrank.java.strings;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TagContentExtractor
@@ -8,12 +9,24 @@ public class TagContentExtractor
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
-		Pattern tagPattern = Pattern.compile("<(.*?)>[\\w\\s]*?<\\/\\1>");
+		Pattern fullPattern = Pattern.compile("<([^<>]+)>[^><]+<\\/\\1>");
+		Pattern contentPattern = Pattern.compile("(?<=>)[^><]+(?=<)");
 		
 		for(int testcases = Integer.parseInt(scanner.nextLine()); testcases > 0; testcases--)
 		{
 			String line = scanner.nextLine();
 			
+			Matcher fullMatcher = fullPattern.matcher(line);
+			
+			if(fullMatcher.matches())
+			{
+				System.out.println(fullMatcher.group());
+				
+				//Matcher contentMatcher = contentPattern.matcher(fullMatcher.group());
+				//System.out.println(contentMatcher.group());
+			}
 		}
+		
+		scanner.close();
 	}
 }
