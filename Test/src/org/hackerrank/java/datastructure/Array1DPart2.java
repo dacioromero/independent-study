79f5d6532c2087f1180fa246eef1a531178279e4
FireClawGames <Dacio_Romero@outlook.com>
2016-05-10 13:31:53 -0400

    Still working on Part 2

--- Test/src/org/hackerrank/java/datastructure/Array1DPart2.java
+++ Test/src/org/hackerrank/java/datastructure/Array1DPart2.java
@@ -1,9 +1,43 @@
 package org.hackerrank.java.datastructure;
 
+import java.util.Scanner;
+
 public class Array1DPart2
 {
 	public static void main(String[] args)
 	{
+		Scanner scanner = new Scanner(System.in);
 		
+		int[][] sequences = new int[scanner.nextInt()][];
+		int[] jumps = new int[sequences.length];
+		
+		for(int i = 0; i < sequences.length; i++)
+		{
+			sequences[i] = new int[scanner.nextInt()];
+			jumps[i] = scanner.nextInt();
+			
+			for(int j = 0; j < sequences[i].length; j++)
+			{
+				sequences[i][j] = scanner.nextInt();
+			}
+		}
+		
+		scanner.close();
+		
+		for(int i = 0; i < sequences.length; i++)
+		{
+			int currentIndex = 0;
+			int currentStart = 0;
+			boolean solveable = false;
+			
+			while(!solveable)
+			{
+				if(sequences[i][currentIndex + jumps[i]] == 0)
+				{
+					currentIndex += jumps[i];
+					
+				}
+			}
+		}
     }
 }
