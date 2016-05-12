c9a7b2faaef0fa3a47229c14e3104c65c48a940f
Dacio Romero <Dacio_Romero@outlook.com>
2016-05-12 00:28:09 -0400

    Removed unused imports.

--- Test/src/org/mcintosh/independentstudy/evaluations/Midterm.java
+++ Test/src/org/mcintosh/independentstudy/evaluations/Midterm.java
@@ -1,8 +1,6 @@
 package org.mcintosh.independentstudy.evaluations;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Midterm 
