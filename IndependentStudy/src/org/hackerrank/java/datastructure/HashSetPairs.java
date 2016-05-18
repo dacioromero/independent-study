8145ad99f429015a3b11436d63f77d8142b7204c
FireClawGames <Dacio_Romero@outlook.com>
2016-05-18 13:34:08 -0400

    Working on HashSet

---
+++ IndependentStudy/src/org/hackerrank/java/datastructure/HashSetPairs.java
@@ -0,0 +1,23 @@
+package org.hackerrank.java.datastructure;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HashSetPairs
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		HashSet<String> pairSet = new HashSet<String>();
+		
+		for(int i = Integer.parseInt(scanner.nextLine()); i > 0; i++)
+		{
+			pairSet.add(scanner.nextLine());
+			
+			System.out.println(pairSet.size());
+		}
+		
+		scanner.close();
+	}
+}
--- IndependentStudy/src/org/hackerrank/java/datastructure/Stack.java
+++
@@ -1,9 +0,0 @@
-package org.hackerrank.java.datastructure;
-
-public class Stack
-{
-	public static void main(String[] args)
-	{
-		
-	}
-}
