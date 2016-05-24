cd1e002aef601130d5d78b9bbfcdb789804aeff4
Dacio Romero <Dacio_Romero@outlook.com>
2016-05-23 20:35:53 -0400

    Renamed classes and finished generics.

---
+++ IndependentStudy/src/org/hackerrank/java/datastructure/Arraylist.java
@@ -0,0 +1,41 @@
+package org.hackerrank.java.datastructure;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Arraylist
+{
+	public static void main(String[] args)
+	{
+		ArrayList<ArrayList<Integer>> integers = new ArrayList<ArrayList<Integer>>();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i = scanner.nextInt(); i > 0; i--)
+		{
+			ArrayList<Integer> line = new ArrayList<Integer>();
+			
+			for(int j = scanner.nextInt(); j > 0; j--)
+			{
+				line.add(scanner.nextInt());
+			}
+			
+			integers.add(line);
+		}
+		
+		for(int i = scanner.nextInt(); i > 0; i--)
+		{
+			try
+			{
+				System.out.println(integers.get(scanner.nextInt() - 1).get(scanner.nextInt() - 1));
+			}
+			
+			catch(Exception e)
+			{
+				System.out.println("ERROR!");
+			}
+		}
+		
+		scanner.close();
+	}
+}
--- IndependentStudy/src/org/hackerrank/java/datastructure/ArraylistIteration.java
+++
@@ -1,9 +0,0 @@
-package org.hackerrank.java.datastructure;
-
-public class ArraylistIteration
-{
-	public static void main(String[] args)
-	{
-		ArrayList 
-	}
-}
