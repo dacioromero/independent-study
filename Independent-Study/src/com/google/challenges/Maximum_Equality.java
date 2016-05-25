37ede9ca87c6817a20005026370508a9ca9452bf
FireClawGames <Dacio_Romero@outlook.com>
2016-05-25 13:33:22 -0400

    Working on equality

---
+++ Independent-Study/src/com/google/challenges/Maximum_Equality.java
@@ -0,0 +1,15 @@
+package com.google.challenges;
+
+public class Maximum_Equality
+{   
+	public static void main(String[] args)
+	{
+		System.out.println(answer(new int[] {1, 4, 1}));
+		System.out.println(answer(new int[] {1, 2}));
+	}
+	
+    public static int answer(int[] x)
+    { 
+    	return 0;
+    }
+}
