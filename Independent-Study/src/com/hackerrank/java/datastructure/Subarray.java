981e22b199463b8d84d778dd5d335f64fdfc941c
FireClaw <Dacio_Romero@outlook.com>
2016-10-27 16:09:03 -0400

    Renamed files to be consistent with HackerRank.
    Renamed:
    - "Array1DPart1.java" to "Subarray.java"
    - "PhoneMap.java" to "MapPhone.java" for consistency

---
+++ Independent-Study/src/com/hackerrank/java/datastructure/Subarray.java
@@ -0,0 +1,33 @@
+package com.hackerrank.java.datastructure;
+
+import java.util.Scanner;
+
+public class Subarray {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int[] integers = new int[scanner.nextInt()];
+
+		for (int i = 0; i < integers.length; i++) {
+			integers[i] = scanner.nextInt();
+		}
+
+		scanner.close();
+
+		int negativeSubs = 0;
+
+		for (int i = 0; i < integers.length; i++) {
+			int sum = 0;
+
+			for (int j = i; j < integers.length; j++) {
+				sum += integers[j];
+
+				if (sum < 0) {
+					negativeSubs++;
+				}
+			}
+		}
+
+		System.out.println(negativeSubs);
+	}
+}
